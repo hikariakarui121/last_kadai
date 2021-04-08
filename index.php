2574ab9a823e39cf3dfb14926125bc844fcd889e
hikariakarui121 <80142147+hikariakarui121@users.noreply.github.com>
2021-04-09 03:49:26 +0900

    ツーリング画像共有アプリ（HTML/CSS/JS/PHP)

--- index.php
+++ index.php
@@ -64,29 +64,45 @@ if ($status==false) {
       <p>📍山梨県</P>
     </div>
 
-  <!-- メインの中身を書いていく -->
-  <!-- 画像が表示されない -->
-
+    <!-- メインの中身を書いていく -->
+    <!-- 画像が表示されない -->
+    
     <div class ="main">
       <div class="top">
         <p>・・・旅のしおり・・・<p>
-      </div>
+          </div>
+          
+          <div class="content">
+            <?=$view?>
+          </div>
+        </div>
+        
+        <section id =contents>
+            <div class="title__contents">
+              <div class="title__image">
+                 <img src="./img/sub.jpeg" alt="">
+              </div>
+              <div class="title__text">
+                <h2>キャンプ・ツーリング写真の共有サイト</h2>
+                <p>①新規会員設定で写真を共有<br>
+                   ②キャンプでの設営写真、景色写真、ツーリング写真を選択<br>
+                   ③場所・目的地・印象・シーズンなどについてを共有<br>
+                </p>
+                <div id="title__btn"><a href="touroku.php">投稿する</a></div>
+            </div>
+          </div>
+        </section>
 
-      <div class="content">
-        <?=$view?>
-      </div>
-    </div>
-
-   <!-- ※ここから、フッター -->
-  <footer>
-    <hr>
-    <ul id="info">
-      <li><a href="index.php">home</a></li>
-      <li><a href="">問い合わせ</a></li>
-    </ul>
-    <small>&copy; 2022 tabi*旅ツーサイト.</small>
-  </footer>
-  
-  <script> </script>
-</body>
-</html>
+        <!-- ※ここから、フッター -->
+        <footer>
+          <hr>
+          <ul id="info">
+            <li><a href="index.php">home</a></li>
+            <li><a href="">問い合わせ</a></li>
+          </ul>
+          <small>&copy; 2022 tabi*旅ツーサイト.</small>
+        </footer>
+        
+        <script> </script>
+      </body>
+      </html>
