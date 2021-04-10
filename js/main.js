cb8c1ac24266e8f1f6714da45dc2757bc9d1273d
hikariakarui121 <80142147+hikariakarui121@users.noreply.github.com>
2021-04-10 18:05:24 +0900

    ツーリング画像共有アプリ（HTML/CSS/JS/PHP)

--- js/main.js
+++ js/main.js
@@ -1,52 +1,26 @@
+  const target =document.querySelector('.title__image');
 
-  const btn = document.querySelector('#btn');
-  // const test =document.getElementById('test');
-  const contents = document.querySelector('.btn-1');
+  function callback(entries, obs){
+    console.log(entries[0]);
 
-  contents.addEventListener("click", ()=>{
-    const results = ['グー','チョキ','パー'];
-    const n =Math.floor(Math.random()*results.length);
+    if(entries[0].isIntersecting){
+      // くらすをつける
+      entries[0].target.classList.add('appear');
+    }else{
+      // クラスを外す
+      // entries[0].target.classList.remove('appear');
+      return
+    }
 
+    // 処理を終了させる処理。
+    obs.unobserve(entries[0].target);
+  }
 
-      if(n==0){
-        // btn.textContent=results[0];
-        document.getElementById("test").src="image/1.jpg";
-        // btn.addClass("inview");
-        // document.getElementById('test').src="image/1.jpg"
-        // console.log(test)
-        // test.src="image/3.jpg"
-
-      }else if(n==1){
-        // btn.textContent=results[1];
-        document.getElementById("test").src="image/2.jpg";
-
-        // btn.addClass("inview2");
-        // document.getElementById('test').src="image/2.jpg"
+  const options ={
+    threshold:[0.1],
+  }
   
-      }else{
-        // btn.textContent=results[2];
-        document.getElementById("test").src="image/3.jpg";
+  const observer = new IntersectionObserver(callback, options);
 
-        // btn.addClass("inview3");
-        // document.getElementById('test').src="image/3.jpg"
-      }
-  });
-
-  // if(n===1){
-  //   btn.write('<img src="image/1.jpg"></img>');
-  // }
-
-
-
-  // switch(n){
-  //   case 0:
-  //     btn.textContent ="グー";
-  //     break;
-  //   case 1:
-  //     btn.textContent ="チョキ";
-  //     break;
-  //   case 2:
-  //     btn.textContent ="パー";
-  //     break;
-
-  // }
+  observer.observe(target);
+  
